361bde71bfc238eaea5723181c964fff885692c5
Kauan Markes <daluzmarkes@gmail.com>
2023-08-30 23:08:01 -0400

    pfvr

--- TrabalhoXML-AGRVAI/Forms/TelaCliente.cs
+++ TrabalhoXML-AGRVAI/Forms/TelaCliente.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Serialization;
 using TrabalhoXML_AGRVAI.Classes;
-using System.Collections.Generic;
+
 using System.Xml;
 using System.Xml.Linq;
 
